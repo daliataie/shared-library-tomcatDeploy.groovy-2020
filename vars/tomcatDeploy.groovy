3404e7123938174b927643653c384fa437d661ff
daliataie <89037651+daliataie@users.noreply.github.com>
2022-01-24 14:19:06 -0600

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,1 +1,10 @@
-
+def call(credId,tomcatIp,tomcatUser){
+  sshagent([credId]) {
+    // copy war file to tomcat server
+    sh """
+        scp -o StrictHostKeyChecking=no target/myweb*.war ${tomcatUser}@${tomcatIp}:/opt/tomcat8/webapps/myweb.war
+        ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/shutdown.sh
+        ssh ${tomcatUser}@${tomcatIp} /opt/tomcat8/bin/startup.sh
+    """
+  }
+}
